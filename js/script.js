27be5d7bcb92a9b1d25f1ee4db6f6716eabe880e
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-08-29 16:45:18 +0100

    get recipies

--- js/script.js
+++ js/script.js
@@ -3,7 +3,35 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      message: 'Hello Vue!'
+        search_value: '',
+        recipes: [],
+        api_url: 'https://www.themealdb.com/api/json/v1/1/search.php?s='
     }
-  }
+  },
+  methods: {
+    getRecipies(search){
+        fetch(`${api_url}${search}`)
+        .then(response => response.json())
+        .then(data => {
+            this.recipes = data.meals
+            this.clearSearch()
+        })
+        .catch(error => console.log(error))
+    },
+    clearRecipies(){
+        this.recipes = []
+    },
+    validateSearch(){
+        if(this.search_value.length > 0){
+            this.getRecipies(this.search_value)
+        }else{
+            this.clearRecipies()
+            alert('Please enter a search term')
+        }
+    },
+    clearSearch(){
+        this.search_value = ''
+        this.clearRecipies()
+    }
+  },
 }).mount('#app')
