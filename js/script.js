9756e0c997cf29d2515dab44c80c47e0cb31c556
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-08-29 17:31:21 +0100

    get data

--- js/script.js
+++ js/script.js
@@ -1,61 +1,61 @@
 const { createApp } = Vue
 
 createApp({
-  data() {
-    return {
-        search_value: '',
-        recipes: [],
-        api_url: 'https://www.themealdb.com/api/json/v1/1/search.php?s='
-    }
-  },
-  methods: {
-    getRecipies(url, search){
-        fetch(`${url}${search}`)
-        .then(response => response.json())
-        .then(data => {
-            data.meals.forEach(meal => {
-                this.recipes.push({
-                    id: meal.idMeal,
-                    name: meal.strMeal,
-                    image: meal.strMealThumb,
-                    category: meal.strCategory,
-                    area: meal.strArea,
-                    instructions: meal.strInstructions,
-                    ingredients: [
-                        meal.strIngredient1,
-                        meal.strIngredient2,
-                        meal.strIngredient3,
-                        meal.strIngredient4,
-                        meal.strIngredient5,
-                        meal.strIngredient6,
-                        meal.strIngredient7,
-                        meal.strIngredient8,
-                        meal.strIngredient9,
-                        meal.strIngredient10,
-                    ],
-                    measures: [
-                        meal.strMeasure1,
-                        meal.strMeasure2,
-                        meal.strMeasure3,
-                        meal.strMeasure4,
-                        meal.strMeasure5,
-                    ]
-                })
-            })
-            console.log(this.recipes)
-            this.clearSearch()
-        })
-        .catch(error => console.log(error))
-    },
-    validateSearch(){
-        if(this.search_value.length > 0){
-            this.getRecipies(this.api_url, this.search_value)
-        }else{
-            alert('Please enter a search term')
+    data() {
+        return {
+            search_value: '',
+            recipes: [],
+            api_url: 'https://www.themealdb.com/api/json/v1/1/search.php?s='
         }
     },
-    clearSearch(){
-        this.search_value = ''
-    }
-  },
+    methods: {
+        getRecipies(url, search) {
+            fetch(`${url}${search}`)
+                .then(response => response.json())
+                .then(data => {
+                    data.meals.forEach(meal => {
+                        this.recipes.push({
+                            id: meal.idMeal,
+                            name: meal.strMeal,
+                            image: meal.strMealThumb,
+                            category: meal.strCategory,
+                            area: meal.strArea,
+                            instructions: meal.strInstructions,
+                            ingredients: [
+                                meal.strIngredient1,
+                                meal.strIngredient2,
+                                meal.strIngredient3,
+                                meal.strIngredient4,
+                                meal.strIngredient5,
+                                meal.strIngredient6,
+                                meal.strIngredient7,
+                                meal.strIngredient8,
+                                meal.strIngredient9,
+                                meal.strIngredient10,
+                            ],
+                            measures: [
+                                meal.strMeasure1,
+                                meal.strMeasure2,
+                                meal.strMeasure3,
+                                meal.strMeasure4,
+                                meal.strMeasure5,
+                            ]
+                        })
+                    })
+                    console.log(this.recipes)
+                    this.clearSearch()
+                })
+                .catch(error => console.log(error))
+        },
+        validateSearch() {
+            if (this.search_value.length > 0) {
+                this.getRecipies(this.api_url, this.search_value)
+            } else {
+                alert('Please enter a search term')
+            }
+        },
+        clearSearch() {
+            this.search_value = ''
+        }
+    },
 }).mount('#app')
